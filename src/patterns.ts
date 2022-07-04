19b3c69788d3d6154a525d8a6a9b0540bf5a9a36
John Anderson Muniz <johnandersonmuniz@gmail.com>
2022-07-04 18:00:06 -0300

    Turn block, line, para, sentence and word patterns into factories
    Accept a prefix and a suffix regex in them, creating a base general
    formula that can be used independently or where their "value"
    counterparts can be inserted.

--- src/patterns.ts
+++ src/patterns.ts
@@ -13,32 +13,39 @@ export const arrDiv = /(?:\r?\n|\s*,\s*)/;
 
 export const falsy = new RegExp(`(?:${falsyChars.join("|")})+|false`, "g");
 
-export const block = "([\\s\\S]*?)[ ]*$";
+export const block = (prefix: string = "[ ]*", suffix: string = "[ ]*") =>
+    `${prefix}([\\s\\S]*?)${suffix}$`;
 
 export const blockValue = (key: string): RegExp =>
-    new RegExp(intro(key) + block, "i");
+    new RegExp(block(intro(key)), "i");
 
 export const intro = (key: string) =>
     key ? `(?<=^|\\s)${dec(key)}${div}` : "";
 
-export const line = "(.*)[ ]*";
+export const line = (prefix: string = "[ ]*", suffix: string = "[ ]*") =>
+    `${prefix}(.*)${suffix}`;
 
 export const lineValue = (key: string): RegExp =>
-    new RegExp(intro(key) + line, "i");
+    new RegExp(line(intro(key)), "i");
 
-export const para = "([\\s\\S]*?)[ ]*(?:[\r\n]+.*" + div + "|$)";
+export const para = (
+    prefix: string = "[ ]*",
+    suffix: string = "[ ]*(?:[\r\n]+|$)"
+) => `${prefix}([\\s\\S]*?)${suffix}`;
 
 export const paraValue = (key: string): RegExp =>
-    new RegExp(intro(key) + para, "i");
+    new RegExp(para(intro(key), `[ ]*(?:[\r\n]+.*${div}|$)`), "i");
 
-export const sentence = "([^,]*)[ ]*";
+export const sentence = (prefix: string = "[ ]*", suffix: string = "[ ]*") =>
+    `${prefix}([^,]*)${suffix}`;
 
 export const sentenceValue = (key: string): RegExp =>
-    new RegExp(intro(key) + sentence, "i");
+    new RegExp(sentence(intro(key)), "i");
 
 export const truthy = new RegExp(`(?:${truthyChars.join("|")})+|true`, "g");
 
-export const word = "([^\\s]*)[ ]*";
+export const word = (prefix: string = "[ ]*", suffix: string = "[ ]*") =>
+    `${prefix}([^\\s]*)${suffix}`;
 
 export const wordValue = (key: string): RegExp =>
-    new RegExp(intro(key) + word, "i");
+    new RegExp(word(intro(key)), "i");
