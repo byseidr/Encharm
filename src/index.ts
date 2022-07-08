c6b884f9644f363efc3db603d30634a23297c6dc
John Anderson Muniz <johnandersonmuniz@gmail.com>
2022-07-08 14:51:54 -0300

    Add core arg to all charms and suffix + core to "value" charms

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,20 @@ import * as patterns from "./patterns";
 export const arr = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string[] => {
-    let result: string = para(content, prefix, suffix);
+    let result: string = para(content, prefix, suffix, core);
     return result?.split?.(patterns.arrDiv)?.filter?.(Boolean) ?? [];
 };
 
-export const arrValue = (key: string, content: string): string[] => {
-    let result: string = paraValue(key, content);
+export const arrValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string[] => {
+    let result: string = paraValue(key, content, suffix, core);
     return result?.split?.(patterns.arrDiv)?.filter?.(Boolean) ?? [];
 };
 
@@ -21,9 +27,10 @@ export const bool = (
     content: string,
     string: boolean = false,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string | boolean => {
-    content = line(content, prefix, suffix);
+    content = line(content, prefix, suffix, core);
     return string
         ? helpers.getStringBool(content)
         : helpers.getTrueBool(content);
@@ -32,9 +39,11 @@ export const bool = (
 export const boolValue = (
     key: string,
     content: string,
-    string: boolean = false
+    string: boolean = false,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string | boolean => {
-    content = lineValue(key, content);
+    content = lineValue(key, content, suffix, core);
     return string
         ? helpers.getStringBool(content)
         : helpers.getTrueBool(content);
@@ -43,61 +52,75 @@ export const boolValue = (
 export const days = (
     content: string | number,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: number | null = int(content, prefix, suffix);
+    let result: number | null = int(content, prefix, suffix, core);
     return result && result === result ? result * 1440 : null;
 };
 
 export const daysValue = (
     key: string,
-    content: string | number
+    content: string | number,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: number | null = intValue(key, content);
+    let result: number | null = intValue(key, content, suffix, core);
     return result && result === result ? result * 1440 : null;
 };
 
 export const block = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
-): string => helpers.getMatch(content, patterns.block(prefix, suffix));
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => helpers.getMatch(content, patterns.block(prefix, suffix, core));
 
-export const blockValue = (key: string, content: string): string =>
-    helpers.getMatch(content, patterns.blockValue(key));
+export const blockValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => helpers.getMatch(content, patterns.blockValue(key, suffix, core));
 
 export const hours = (
     content: string | number,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: number | null = int(content, prefix, suffix);
+    let result: number | null = int(content, prefix, suffix, core);
     return result && result === result ? result * 60 : null;
 };
 
 export const hoursValue = (
     key: string,
-    content: string | number
+    content: string | number,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: number | null = intValue(key, content);
+    let result: number | null = intValue(key, content, suffix, core);
     return result && result === result ? result * 60 : null;
 };
 
 export const int = (
     content: string | number,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: string | number = line(<string>content, prefix, suffix);
+    let result: string | number = line(<string>content, prefix, suffix, core);
     result = Math.ceil(Number(content));
     return result && result === result ? result : null;
 };
 
 export const intValue = (
     key: string,
-    content: string | number
+    content: string | number,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: string | number = lineValue(key, <string>content);
+    let result: string | number = lineValue(key, <string>content, suffix, core);
     result = Math.ceil(Number(content));
     return result && result === result ? result : null;
 };
@@ -105,16 +128,25 @@ export const intValue = (
 export const line = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string => {
-    let result = helpers.getMatch(content, patterns.line(prefix, suffix));
+    let result = helpers.getMatch(content, patterns.line(prefix, suffix, core));
     result = /^\d+$/.test(result) ? +result : result;
     result = result?.trim?.() ?? result;
     return result;
 };
 
-export const lineValue = (key: string, content: string): string => {
-    let result = helpers.getMatch(content, patterns.lineValue(key));
+export const lineValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => {
+    let result = helpers.getMatch(
+        content,
+        patterns.lineValue(key, suffix, core)
+    );
     result = result?.trim?.() ?? result;
     return result;
 };
@@ -122,15 +154,21 @@ export const lineValue = (key: string, content: string): string => {
 export const slug = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string => {
-    let result: string = line(content, prefix, suffix);
+    let result: string = line(content, prefix, suffix, core);
     if (result) result = slugify(result);
     return result;
 };
 
-export const slugValue = (key: string, content: string): string => {
-    let result: string = lineValue(key, content);
+export const slugValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => {
+    let result: string = lineValue(key, content, suffix, core);
     if (result) result = slugify(result);
     return result;
 };
@@ -138,24 +176,41 @@ export const slugValue = (key: string, content: string): string => {
 export const para = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
-): string => helpers.getMatch(content, patterns.para(prefix, suffix));
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => helpers.getMatch(content, patterns.para(prefix, suffix, core));
 
-export const paraValue = (key: string, content: string): string =>
-    helpers.getMatch(content, patterns.paraValue(key));
+export const paraValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => helpers.getMatch(content, patterns.paraValue(key, suffix, core));
 
 export const sentence = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string => {
-    let result = helpers.getMatch(content, patterns.sentence(prefix, suffix));
+    let result = helpers.getMatch(
+        content,
+        patterns.sentence(prefix, suffix, core)
+    );
     result = result?.trim?.() ?? result;
     return result;
 };
 
-export const sentenceValue = (key: string, content: string): string => {
-    let result = helpers.getMatch(content, patterns.sentenceValue(key));
+export const sentenceValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => {
+    let result = helpers.getMatch(
+        content,
+        patterns.sentenceValue(key, suffix, core)
+    );
     result = result?.trim?.() ?? result;
     return result;
 };
@@ -163,15 +218,24 @@ export const sentenceValue = (key: string, content: string): string => {
 export const string = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string => {
-    let result = helpers.getMatch(content, patterns.line(prefix, suffix));
+    let result = helpers.getMatch(content, patterns.line(prefix, suffix, core));
     result = result?.trim?.() ?? result;
     return result;
 };
 
-export const stringValue = (key: string, content: string): string => {
-    let result = helpers.getMatch(content, patterns.lineValue(key));
+export const stringValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => {
+    let result = helpers.getMatch(
+        content,
+        patterns.lineValue(key, suffix, core)
+    );
     result = result?.trim?.() ?? result;
     return result;
 };
@@ -179,15 +243,21 @@ export const stringValue = (key: string, content: string): string => {
 export const url = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string => {
-    let result: string = line(content, prefix, suffix);
+    let result: string = line(content, prefix, suffix, core);
     result = /https?:\/\//.test(result) ? result : "";
     return result;
 };
 
-export const urlValue = (key: string, content: string): string => {
-    let result: string = lineValue(key, content);
+export const urlValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => {
+    let result: string = lineValue(key, content, suffix, core);
     result = /https?:\/\//.test(result) ? result : "";
     return result;
 };
@@ -195,32 +265,44 @@ export const urlValue = (key: string, content: string): string => {
 export const weeks = (
     content: string | number,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: number | null = int(content, prefix, suffix);
+    let result: number | null = int(content, prefix, suffix, core);
     return result && result === result ? result * 10080 : null;
 };
 
 export const weeksValue = (
     key: string,
-    content: string | number
+    content: string | number,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): number | null => {
-    let result: number | null = intValue(key, content);
+    let result: number | null = intValue(key, content, suffix, core);
     return result && result === result ? result * 10080 : null;
 };
 
 export const word = (
     content: string,
     prefix: RegExp | undefined = undefined,
-    suffix: RegExp | undefined = undefined
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
 ): string => {
-    let result = helpers.getMatch(content, patterns.word(prefix, suffix));
+    let result = helpers.getMatch(content, patterns.word(prefix, suffix, core));
     result = result?.trim?.() ?? result;
     return result;
 };
 
-export const wordValue = (key: string, content: string): string => {
-    let result = helpers.getMatch(content, patterns.wordValue(key));
+export const wordValue = (
+    key: string,
+    content: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): string => {
+    let result = helpers.getMatch(
+        content,
+        patterns.wordValue(key, suffix, core)
+    );
     result = result?.trim?.() ?? result;
     return result;
 };
--- src/patterns.ts
+++ src/patterns.ts
@@ -13,39 +13,67 @@ export const arrDiv = /(?:\r?\n|\s*,\s*)/;
 
 export const falsy = new RegExp(`(?:${falsyChars.join("|")})+|false`, "g");
 
-export const block = (prefix: RegExp = /[ ]*/, suffix: RegExp = /[ ]*/) =>
-    new RegExp(`${prefix.source}([\\s\\S]*?)${suffix.source}$`, "i");
+export const block = (
+    prefix: RegExp = /[ ]*/,
+    suffix: RegExp = /[ ]*/,
+    core: RegExp = /([\\s\\S]*?)/
+) => new RegExp(`${prefix.source}${core.source}${suffix.source}$`, "i");
 
-export const blockValue = (key: string): RegExp =>
-    new RegExp(block(intro(key)), "i");
+export const blockValue = (
+    key: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): RegExp => new RegExp(block(intro(key), suffix, core), "i");
 
 export const intro = (key: string): RegExp =>
     new RegExp(key ? `(?<=^|\\s)${dec(key)}${div}` : "");
 
-export const line = (prefix: RegExp = /[ ]*/, suffix: RegExp = /[ ]*/) =>
-    new RegExp(`${prefix.source}(.*)${suffix.source}`, "i");
+export const line = (
+    prefix: RegExp = /[ ]*/,
+    suffix: RegExp = /[ ]*/,
+    core: RegExp = /(.*)/
+) => new RegExp(`${prefix.source}${core.source}${suffix.source}`, "i");
 
-export const lineValue = (key: string): RegExp =>
-    new RegExp(line(intro(key)), "i");
+export const lineValue = (
+    key: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): RegExp => new RegExp(line(intro(key), suffix, core), "i");
 
 export const para = (
     prefix: RegExp = /[ ]*/,
-    suffix: RegExp = /[ ]*(?:[\r\n]+|$)/
-) => new RegExp(`${prefix.source}([\\s\\S]*?)${suffix.source}`, "i");
+    suffix: RegExp = /[ ]*(?:[\r\n]+|$)/,
+    core: RegExp = /([\\s\\S]*?)/
+) => new RegExp(`${prefix.source}${core.source}${suffix.source}`, "i");
 
-export const paraValue = (key: string): RegExp =>
-    new RegExp(para(intro(key), new RegExp(`[ ]*(?:[\r\n]+.*${div}|$)`)), "i");
+export const paraValue = (
+    key: string,
+    suffix: RegExp = new RegExp(`[ ]*(?:[\r\n]+.*${div}|$)`),
+    core: RegExp | undefined = undefined
+): RegExp => new RegExp(para(intro(key), suffix, core), "i");
 
-export const sentence = (prefix: RegExp = /[ ]*/, suffix: RegExp = /[ ]*/) =>
-    new RegExp(`${prefix.source}([^,]*)${suffix.source}`, "i");
+export const sentence = (
+    prefix: RegExp = /[ ]*/,
+    suffix: RegExp = /[ ]*/,
+    core: RegExp = /([^,]*)/
+) => new RegExp(`${prefix.source}${core.source}${suffix.source}`, "i");
 
-export const sentenceValue = (key: string): RegExp =>
-    new RegExp(sentence(intro(key)), "i");
+export const sentenceValue = (
+    key: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): RegExp => new RegExp(sentence(intro(key), suffix, core), "i");
 
 export const truthy = new RegExp(`(?:${truthyChars.join("|")})+|true`, "g");
 
-export const word = (prefix: RegExp = /[ ]*/, suffix: RegExp = /[ ]*/) =>
-    new RegExp(`${prefix.source}([^\\s]*)${suffix.source}`, "i");
+export const word = (
+    prefix: RegExp = /[ ]*/,
+    suffix: RegExp = /[ ]*/,
+    core: RegExp = /([^\\s]*)/
+) => new RegExp(`${prefix.source}${core.source}${suffix.source}`, "i");
 
-export const wordValue = (key: string): RegExp =>
-    new RegExp(word(intro(key)), "i");
+export const wordValue = (
+    key: string,
+    suffix: RegExp | undefined = undefined,
+    core: RegExp | undefined = undefined
+): RegExp => new RegExp(word(intro(key), suffix, core), "i");
